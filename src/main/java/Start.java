2fbcdbe83c786872313c1afa734ad4c4751be52e
ImXianyu <2030752250@qq.com>
2023-08-13 08:46:24 +0800

    Fixed GuiOverlayDebug issues

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Start {
     public static void main(String[] args) {
-        Main.main(concat(new String[]{"--version", "mcp", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
+        Main.main(concat(new String[]{"--version", "MCP-1.8.9-Maven", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
     }
 
     public static <T> T[] concat(T[] first, T[] second) {
